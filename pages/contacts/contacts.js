3090215ed2b85f6af313a0ad05524c02d96d6f9e
sarinzhiev <sarinjievarsen@gmail.com>
2023-05-06 15:48:43 +0600

    logic contact

---
+++ pages/contacts/contacts.js
@@ -0,0 +1,39 @@
+let btnMassage = document.querySelector(".btn__massage")
+let inputMassage = document.querySelector(".input__massage")
+let btnSand = document.querySelector(".btn__sand")
+let form = document.querySelector(".form")
+let email = document.querySelector(".email")
+let phone = document.querySelector(".phone")
+let name = document.querySelector(".name")
+
+
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+
+
+    const resetForm = () => {
+        name.value = ""
+        phone.value = ""
+        email.value = ""
+        inputMassage.value = ""
+    }
+        fetch("http://localhost:8080/message" , {
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({
+                nickname: e.target[0].value,
+                email: e.target[1].value,
+                phone: e.target[2].value,
+                sand : e.target[3].value
+            })
+        }) .then((res)=>{
+            console.log(res )
+            resetForm()
+        }) .catch((err)=>{
+            console.log(err)
+        })
+})
